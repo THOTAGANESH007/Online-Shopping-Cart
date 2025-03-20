f0704995b68d8240dd1bd3e92ce5383089e9ebd9
THOTAGANESH007 <rr200007@rguktrkv.ac.in>
2025-03-20 09:01:16 +0530

    UserController completed

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,57 @@
+import sendEmail from "../config/sendEmail.js";
+import UserModel from "../models/user.js";
+import bcryptjs from "bcryptjs";
+import verifyEmailTemplate from "../utils/verifyEmailTemplate.js";
+export async function registerUserController(req, res) {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password)
+      return res.status(400).json({
+        message: "Please Provide Name or Email or Password",
+        error: true,
+        success: false,
+      });
+
+    const user = UserModel.findOne({ email });
+    if (user) {
+      return res
+        .status(400)
+        .json({ message: "User Already Exists", error: true, success: false });
+    }
+
+    const salt = await bcryptjs.genSalt(7);
+    const hashPassword = await bcryptjs.hash(password, salt);
+    const payload = {
+      //formatting
+      name,
+      email,
+      password: hashPassword,
+    };
+
+    const newUser = new UserModel(payload);
+    const save = await newUser.save();
+    const verifyEmailUrl = `${process.env.FRONTEND_URL}/verify-email?code=${save._id}`;
+
+    const verifyEmail = await sendEmail({
+      sendTo: email,
+      subject: "Verify email from magiccu",
+      html: verifyEmailTemplate({
+        name,
+        url: verifyEmailUrl,
+      }),
+    });
+
+    return res.json({
+      message: "User Registration Successful",
+      error: false,
+      success: true,
+      data: save,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+      error: true,
+      success: false,
+    });
+  }
+}

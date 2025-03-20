4f31c8586f99b3d44647b5d21cebfddcf3b2d578
THOTAGANESH007 <rr200007@rguktrkv.ac.in>
2025-03-20 11:22:21 +0530

    Added Access Tokens

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -2,6 +2,8 @@ import sendEmail from "../config/sendEmail.js";
 import UserModel from "../models/user.js";
 import bcryptjs from "bcryptjs";
 import verifyEmailTemplate from "../utils/verifyEmailTemplate.js";
+import generateAccessToken from "../utils/generateAccessToken.js";
+import generateRefreshToken from "../utils/generateRefreshToken.js";
 export async function registerUserController(req, res) {
   try {
     const { name, email, password } = req.body;
@@ -56,6 +58,96 @@ export async function registerUserController(req, res) {
   } catch (error) {
     return res.status(500).json({
       message: error.message,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+export async function verifyEmailController(req, res) {
+  try {
+    const { code } = req.body;
+    const user = await UserModel.findOne({ _id: code });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Invalid Message", error: true, success: false });
+    }
+    const updateUser = await UserModel.updateOne(
+      { _id: code },
+      { verify_email: true }
+    );
+    return res.json({
+      message: "Email Verification Done",
+      success: true,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      success: false,
+      error: true,
+    });
+  }
+}
+
+export async function loginController(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password)
+      return res.status(400).json({
+        message: "Missing Email or Password fiels",
+        error: true,
+        success: false,
+      });
+    const existedUser = await UserModel.findOne({ email });
+    if (!existedUser)
+      return res.status(400).json({
+        message: "User Doesnot exist",
+        error: true,
+        success: false,
+      });
+
+    if (existedUser.status !== "Active")
+      return res
+        .status(400)
+        .json({ message: "Contact To Admin", error: true, success: flase });
+
+    const checkPassword = await bcryptjs.compare(
+      password,
+      existedUser.password
+    );
+    if (!checkPassword)
+      return res.status(400).json({
+        message: "Check Your Password",
+        error: true,
+        success: false,
+      });
+    const accessToken = await generateAccessToken(existedUser._id);
+    const refreshToken = await generateRefreshToken(existedUser._id);
+
+    const cookiesOption = {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    };
+
+    res.cookie("accessToken", accessToken, cookiesOption);
+    res.cookie("refreshToken", refreshToken, cookiesOption);
+
+    return res.json({
+      message: "Login Successful!!!",
+      success: true,
+      error: false,
+      data: {
+        accessToken,
+        refreshToken,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
       error: true,
       success: false,
     });
--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
-import { registerUserController } from "../controllers/userController.js";
+import {
+  loginController,
+  registerUserController,
+  verifyEmailController,
+} from "../controllers/userController.js";
 const userRouter = Router();
-userRouter.post("/register", registerUserController);
 
+userRouter.post("/register", registerUserController);
+userRouter.post("verify-email", verifyEmailController);
+userRouter.post("/login", loginController);
 export default userRouter;
